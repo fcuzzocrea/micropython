0065407f96edfb17157daa3deff9b16111c9ef42
Damien George <damien.p.george@gmail.com>
2016-03-10 07:35:03 -0500

    leon: Add leon-ex-manager example.

---
+++ leon-ex-manager/run-test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+RM=/bin/rm
+outfile=vmmanage_test.out
+expfile=vmmanage_test.exp
+
+echo "Running test: VM_MANAGER"
+
+make run | grep "^UART A sent : " > $outfile
+
+diff --brief $outfile $expfile > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "test passed"
+    $RM $outfile
+else
+    echo "test failed"
+fi
