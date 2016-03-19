042c80ddb05a7ccbefeb063914cd71b83049450b
Damien George <damien.p.george@gmail.com>
2016-03-19 12:17:35 +0000

    leon: In RTEMS_QUEUE_01, add tests for new queue.receive API.

--- leon-tests/leon_t1/RTEMS_QUEUE_01.py
+++ leon-tests/leon_t1/RTEMS_QUEUE_01.py
@@ -2,6 +2,7 @@
 # Test ID: RTEMS_QUEUE_01
 
 import micropython
+import rtems
 from rtems import queue
 
 try:
@@ -11,6 +12,8 @@ except ValueError as er:
 
 q = queue.create('qqqq', 4, 10)
 buf = bytearray(10)
+
+er = None # global variable needed for try/except below
 
 micropython.heap_lock()
 
@@ -23,3 +26,15 @@ buf[0] = ord('M')
 q.send(buf)
 print(q.receive(buf))
 print(buf)
+
+# test non-blocking receive
+try:
+    q.receive(buf, rtems.NO_WAIT)
+except OSError as er:
+    print('OSError:', er)
+
+# test blocking receive with a small timeout
+try:
+    q.receive(buf, rtems.WAIT, 100)
+except OSError as er:
+    print('OSError:', er)
