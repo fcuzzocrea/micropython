e2c7816c9b3eb51a3ccfe1ed50358920a90199ec
Damien George <damien.p.george@gmail.com>
2017-04-10 15:24:10 +1000

    leon: Update set of CORE_MISC tests from mainline.

--- leon-tests/CORE_MISC/recursive_iternext.py
+++ leon-tests/CORE_MISC/recursive_iternext.py
@@ -1,8 +1,10 @@
 # This tests that recursion with iternext doesn't lead to segfault.
 
+N = 200
+
 try:
     x = (1, 2)
-    for i in range(200):
+    for i in range(N):
         x = enumerate(x)
     tuple(x)
 except RuntimeError:
@@ -10,7 +12,7 @@ except RuntimeError:
 
 try:
     x = (1, 2)
-    for i in range(200):
+    for i in range(N):
         x = filter(None, x)
     tuple(x)
 except RuntimeError:
@@ -18,7 +20,7 @@ except RuntimeError:
 
 try:
     x = (1, 2)
-    for i in range(200):
+    for i in range(N):
         x = map(max, x, ())
     tuple(x)
 except RuntimeError:
@@ -26,7 +28,7 @@ except RuntimeError:
 
 try:
     x = (1, 2)
-    for i in range(200):
+    for i in range(N):
         x = zip(x)
     tuple(x)
 except RuntimeError:
