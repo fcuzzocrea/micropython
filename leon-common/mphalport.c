97985489408fc7fc1f6b2e9c54ce076128ecc251
Damien George <damien.p.george@gmail.com>
2017-07-25 00:12:48 +1000

    leon: Remove mp_hal_stdin_rx_chr, it's not used.
    This function is only needed if the REPL is used, and this is currently
    unsupported in the LEON/SPARC port.

--- leon-common/mphalport.c
+++ leon-common/mphalport.c
@@ -11,14 +11,6 @@
 #include "py/mpconfig.h"
 #include "py/mphal.h"
 
-// Receive single character
-int mp_hal_stdin_rx_chr(void) {
-    unsigned char c = 0;
-    int r = read(0, &c, 1);
-    (void)r;
-    return c;
-}
-
 // Send string of given length
 void mp_hal_stdout_tx_strn(const char *str, size_t len) {
     int r = write(1, str, len);
