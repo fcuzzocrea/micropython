9002d0f8a6654e753bd4a46414ab9abe36364c1a
Damien George <damien.p.george@gmail.com>
2017-09-20 19:01:38 +1000

    leon: Remove unused logic in float tests since it relies on sys.maxsize.
    sys.maxsize may not be available.

--- leon-tests/CORE_FLOAT/float2int_doubleprec_intbig.py
+++ leon-tests/CORE_FLOAT/float2int_doubleprec_intbig.py
@@ -5,44 +5,12 @@ try:
 except:
     import struct
 
-import sys
-maxsize_bits = 0
-maxsize = sys.maxsize
-while maxsize:
-    maxsize >>= 1
-    maxsize_bits += 1
-
-# work out configuration values
-is_64bit = maxsize_bits > 32
-# 0 = none, 1 = long long, 2 = mpz
-ll_type = None
-if is_64bit:
-    if maxsize_bits < 63:
-        ll_type = 0
-else:
-    if maxsize_bits < 31:
-        ll_type = 0
-if ll_type is None:
-    one = 1
-    if one << 65 < one << 62:
-        ll_type = 1
-    else:
-        ll_type = 2
-ll_type = 2 # force mpz
-
 # This case occurs with time.time() values
-if ll_type != 0:
-    print(int(1418774543.))
-    print("%d" % 1418774543.)
-    if ll_type == 3:
-        print(int(2.**100))
-        print("%d" % 2.**100)
-else:
-    print(int(1073741823.))
-    print("%d" % 1073741823.)
+print(int(1418774543.))
+print("%d" % 1418774543.)
 
 testpass = True
-p2_rng = ((30,63,1024),(62,63,1024))[is_64bit][ll_type]
+p2_rng = 1024
 for i in range(0,p2_rng):
     bitcnt = len(bin(int(2.**i))) - 3;
     if i != bitcnt:
@@ -51,7 +19,7 @@ for i in range(0,p2_rng):
 print("power of  2 test: %s" % (testpass and 'passed' or 'failed'))
 
 testpass = True
-p10_rng = ((9,18,23),(18,18,23))[is_64bit][ll_type]
+p10_rng = 23
 for i in range(0,p10_rng):
     digcnt = len(str(int(10.**i))) - 1;
     if i != digcnt:
@@ -67,31 +35,8 @@ def fp2int_test(num, name, should_fail):
         passed = should_fail
     print('%s: %s' % (name, passed and 'passed' or 'failed'))
 
-if ll_type != 2:
-    if ll_type == 0:
-        if is_64bit:
-            neg_bad_fp = -1.00000005*2.**62.
-            pos_bad_fp = 2.**62.
-            neg_good_fp = -2.**62.
-            pos_good_fp = 0.99999993*2.**62.
-        else:
-            neg_bad_fp = -1.00000005*2.**30.
-            pos_bad_fp = 2.**30.
-            neg_good_fp = -2.**30.
-            pos_good_fp = 0.9999999499*2.**30.
-    else:
-        neg_bad_fp = -0.51*2.**64.
-        pos_bad_fp = 2.**63.
-        neg_good_fp = -2.**63.
-        pos_good_fp = 1.9999998*2.**62.
-
-    fp2int_test(neg_bad_fp, 'neg bad', True)
-    fp2int_test(pos_bad_fp, 'pos bad', True)
-    fp2int_test(neg_good_fp, 'neg good', False)
-    fp2int_test(pos_good_fp, 'pos good', False)
-else:
-    fp2int_test(-1.9999999999999981*2.**1023., 'large neg', False)
-    fp2int_test(1.9999999999999981*2.**1023., 'large pos', False)
+fp2int_test(-1.9999999999999981*2.**1023., 'large neg', False)
+fp2int_test(1.9999999999999981*2.**1023., 'large pos', False)
 
 fp2int_test(float('inf'), 'inf test', True)
 fp2int_test(float('nan'), 'NaN test', True)
--- leon-tests/CORE_FLOAT/float2int_fp30_intbig.py
+++ leon-tests/CORE_FLOAT/float2int_fp30_intbig.py
@@ -5,40 +5,14 @@ try:
 except:
     import struct
 
-import sys
-maxsize_bits = 0
-maxsize = sys.maxsize
-while maxsize:
-    maxsize >>= 1
-    maxsize_bits += 1
-
-# work out configuration values
-is_64bit = maxsize_bits > 32
-# 0 = none, 1 = long long, 2 = mpz
-ll_type = None
-if is_64bit:
-    if maxsize_bits < 63:
-        ll_type = 0
-else:
-    if maxsize_bits < 31:
-        ll_type = 0
-if ll_type is None:
-    one = 1
-    if one << 65 < one << 62:
-        ll_type = 1
-    else:
-        ll_type = 2
-ll_type = 2 # force mpz
-
 # basic conversion
 print(int(14187744.))
 print("%d" % 14187744.)
-if ll_type == 2:
-    print(int(2.**100))
-    print("%d" % 2.**100)
+print(int(2.**100))
+print("%d" % 2.**100)
 
 testpass = True
-p2_rng = ((30,63,127),(62,63,127))[is_64bit][ll_type]
+p2_rng = 127
 for i in range(0,p2_rng):
     bitcnt = len(bin(int(2.**i))) - 3;
     if i != bitcnt:
@@ -64,31 +38,8 @@ def fp2int_test(num, name, should_fail):
         passed = should_fail
     print('%s: %s' % (name, passed and 'passed' or 'failed'))
 
-if ll_type != 2:
-    if ll_type == 0:
-        if is_64bit:
-            neg_bad_fp = -1.00000005*2.**62.
-            pos_bad_fp = 2.**62.
-            neg_good_fp = -2.**62.
-            pos_good_fp = 0.99999993*2.**62.
-        else:
-            neg_bad_fp = -1.00000005*2.**30.
-            pos_bad_fp = 2.**30.
-            neg_good_fp = -2.**30.
-            pos_good_fp = 0.9999999499*2.**30.
-    else:
-        neg_bad_fp = -0.51*2.**64.
-        pos_bad_fp = 2.**63.
-        neg_good_fp = -2.**63.
-        pos_good_fp = 1.9999998*2.**62.
-
-    fp2int_test(neg_bad_fp, 'neg bad', True)
-    fp2int_test(pos_bad_fp, 'pos bad', True)
-    fp2int_test(neg_good_fp, 'neg good', False)
-    fp2int_test(pos_good_fp, 'pos good', False)
-else:
-    fp2int_test(-1.999999879*2.**126., 'large neg', False)
-    fp2int_test(1.999999879*2.**126., 'large pos', False)
+fp2int_test(-1.999999879*2.**126., 'large neg', False)
+fp2int_test(1.999999879*2.**126., 'large pos', False)
 
 fp2int_test(float('inf'), 'inf test', True)
 fp2int_test(float('nan'), 'NaN test', True)
