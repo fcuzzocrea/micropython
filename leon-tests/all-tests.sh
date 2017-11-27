58fec168239bfac0f0c47fd3ed70d734f53a81c4
Damien George <damien.p.george@gmail.com>
2017-11-27 22:54:07 +1100

    leon: Add a simple script to run all the tests.

---
+++ leon-tests/all-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+./run-tests.sh CORE_BASICS/*.py
+./run-tests.sh CORE_UNICODE/*.py
+./run-tests.sh CORE_FLOAT/*.py
+./run-tests.sh CORE_EXTMOD/*.py
+./run-tests.sh CORE_MISC/*.py
+./run-tests.sh CORE_MICROPYTHON/*.py
+./run-tests.sh spacebel_tickets/*.py
+./run-tests.sh leon_t1/*.exp
+./run-tests.sh -t 2 leon_t2/*.exp
+./run-tests.sh -t 10 leon_t10/*.exp
+./run-tests.sh -o leon_perf/*.py
