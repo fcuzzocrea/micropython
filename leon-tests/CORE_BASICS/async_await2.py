ab1a13cd7a76bde2e046836ef3f07af81cc04df5
Damien George <damien.p.george@gmail.com>
2018-02-20 13:45:55 +1100

    leon: Update CORE_BASICS/async_xxx tests to work without attrtuple.

--- leon-tests/CORE_BASICS/async_await2.py
+++ leon-tests/CORE_BASICS/async_await2.py
@@ -1,7 +1,7 @@
 # test await expression
 
 import sys
-if sys.implementation.name == 'micropython':
+if not hasattr(sys.implementation, 'name') or sys.implementation.name == 'micropython':
     # uPy allows normal generators to be awaitables
     coroutine = lambda f: f
 else:
