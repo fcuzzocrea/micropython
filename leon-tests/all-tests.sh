700ca228398af88ccb39e6ad07b1c2f2ff0620aa
Damien George <damien.p.george@gmail.com>
2018-05-08 17:17:43 +1000

    leon: Add cpydiff tests for differences between uPy and CPy.
    From upstream, only those relevant to the LEON port have been taken.
    
    Tests not taken because the compiler isn't included:
    
        syntax_unicode_nameesc.py
    
    Tests not taken because there is no eval/exec:
    
        core_locals_eval.py
        syntax_spaces.py
    
    Tests not taken because there is no file import:
    
        core_import_path.py
        core_import_prereg.py
        core_import_split_ns_pkgs.py
        modules_deque.py
        modules_json_nonserializable.py

--- leon-tests/all-tests.sh
+++ leon-tests/all-tests.sh
@@ -8,6 +8,7 @@
 ./run-tests.sh CORE_MICROPYTHON/*.py
 ./run-tests.sh CORE_STRESS/*.py
 ./run-tests.sh spacebel_tickets/*.py
+./run-tests.sh cpydiff/*.py
 ./run-tests.sh leon_t1/*.exp
 ./run-tests.sh -t 2 leon_t2/*.exp
 ./run-tests.sh -t 10 leon_t10/*.exp
