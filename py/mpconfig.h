3d4fbcaff9555a37f4fdd474404c75624962cbb6
stijn <stijn@ignitron.net>
2019-07-02 10:28:44 +0200

    py/modmath: Implement math.isclose() for non-complex numbers.
    As per PEP 485, this function appeared in for Python 3.5.  Configured via
    MICROPY_PY_MATH_ISCLOSE which is disabled by default, but enabled for the
    ports which already have MICROPY_PY_MATH_SPECIAL_FUNCTIONS enabled.
    
    From upstream MicroPython: af5c998f37ddc62abfd36e0b8be511c392fc25d8

--- py/mpconfig.h
+++ py/mpconfig.h
@@ -988,6 +988,11 @@ typedef double mp_float_t;
 #define MICROPY_PY_MATH_SPECIAL_FUNCTIONS (0)
 #endif
 
+// Whether to provide math.isclose function
+#ifndef MICROPY_PY_MATH_ISCLOSE
+#define MICROPY_PY_MATH_ISCLOSE (0)
+#endif
+
 // Whether to provide "cmath" module
 #ifndef MICROPY_PY_CMATH
 #define MICROPY_PY_CMATH (0)
