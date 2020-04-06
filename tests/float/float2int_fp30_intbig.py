ccbf25469bdf14af011e4a7593d1e0bd81adb4f1
stijn <stijn@ignitron.net>
2020-04-06 10:44:48 +0200

    py/objint: Do not use fpclassify.
    For combinations of certain versions of glibc and gcc the definition of
    fpclassify always takes float as argument instead of adapting itself to
    float/double/long double as required by the C99 standard.  At the time of
    writing this happens for instance for glibc 2.27 with gcc 7.5.0 when
    compiled with -Os and glibc 3.0.7 with gcc 9.3.0.  When calling fpclassify
    with double as argument, as in objint.c, this results in an implicit
    narrowing conversion which is not really correct plus results in a warning
    when compiled with -Wfloat-conversion.  So fix this by spelling out the
    logic manually.
    
    From upstream MicroPython: f31f9a8b70db03cbcbcf39b493f959d0e284962a

--- tests/float/float2int_fp30_intbig.py
+++ tests/float/float2int_fp30_intbig.py
@@ -90,6 +90,7 @@ else:
     fp2int_test(1.999999879*2.**126., 'large pos', False)
 
 fp2int_test(float('inf'), 'inf test', True)
+fp2int_test(float("-inf"), "inf test", True)
 fp2int_test(float('nan'), 'NaN test', True)
 
 # test numbers < 1 (this used to fail; see issue #1044)
