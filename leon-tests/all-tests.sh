cf77ae55e569e7bfa7f8deb4a07821af71075efd
Damien George <dpgeorge@georgerobotics.co.uk>
2022-10-03 14:37:16 +1100

    leon-tests/all-tests.sh: Run micropython-ulab tests as part of script.
    Signed-off-by: Damien George <dpgeorge@georgerobotics.co.uk>

--- leon-tests/all-tests.sh
+++ leon-tests/all-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Core MicroPython tests.
 ./run-tests.sh CORE_BASICS/*.py
 ./run-tests.sh CORE_UNICODE/*.py
 ./run-tests.sh CORE_FLOAT/*.py
@@ -7,10 +8,19 @@
 ./run-tests.sh CORE_MISC/*.py
 ./run-tests.sh CORE_MICROPYTHON/*.py
 ./run-tests.sh CORE_STRESS/*.py
-./run-tests.sh spacebel_tickets/*.py
 ./run-tests.sh cpydiff/*.py
+
+# micropython-ulab tests.
+./run-tests.sh `find ../lib/micropython-ulab/tests -name \*.py | sort`
+
+# LEON and RTEMS specific tests.
+./run-tests.sh spacebel_tickets/*.py
 ./run-tests.sh leon_t1/*.exp
 ./run-tests.sh -t 2 leon_t2/*.exp
 ./run-tests.sh -t 10 leon_t10/*.exp
+
+# LEON performance tests (they have no expected output).
 ./run-tests.sh -o leon_perf/*.py
+
+# mpy-cross tests.
 ./run-tests-mpy-cross.sh mpy_cross/*.py
