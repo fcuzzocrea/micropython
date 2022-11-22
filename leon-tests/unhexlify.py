1dbd87232c23b92c0c6a816003e821a71ce217e0
Damien George <dpgeorge@georgerobotics.co.uk>
2022-11-22 18:46:18 +1100

    leon-tests/unhexlify.py: Parse error output that is not hex output.
    Signed-off-by: Damien George <dpgeorge@georgerobotics.co.uk>

--- leon-tests/unhexlify.py
+++ leon-tests/unhexlify.py
@@ -18,4 +18,23 @@ lines = []
 for line in sys.stdin:
     if line.startswith(prefix):
         lines.append(line[len(prefix) :].rstrip())
+
+# Check the last line for any error messages (non-valid hexlify output).
+error = None
+if lines:
+    last_line = lines[-1]
+    last_valid_index = 0
+    for i in range(0, len(last_line), 2):
+        try:
+            binascii.unhexlify(last_line[i:i+2])
+            last_valid_index = i + 2
+        except TypeError:
+            break
+    if last_valid_index < len(last_line):
+        # Last line has an error message, so extract it.
+        lines[-1] = last_line[: last_valid_index]
+        error = "\nError: " + last_line[last_valid_index:]
+
 sys.stdout.write(binascii.unhexlify("".join(lines)))
+if error:
+    print(error)
