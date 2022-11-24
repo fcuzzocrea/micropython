cb0423ac3520d141c2cf874ed7845031e0484707
Damien George <dpgeorge@georgerobotics.co.uk>
2022-11-24 14:31:06 +1100

    leon-tests/all-tests.sh: Explicitly list all ulab tests.
    The following 3 tests are not listed because they use features which are
    not enabled on LEON (file saving/loading):
    - tests/2d/numpy/load_save.py
    - tests/2d/numpy/loadtxt.py
    - tests/2d/numpy/savetxt.py
    
    Signed-off-by: Damien George <dpgeorge@georgerobotics.co.uk>

--- leon-tests/all-tests.sh
+++ leon-tests/all-tests.sh
@@ -1,5 +1,71 @@
 #!/bin/sh
 
+# Define the ulab tests to run.  They should all pass.
+ulab_test_dir=../lib/micropython-ulab/tests
+ulab_tests="
+    $ulab_test_dir/1d/complex/complex_exp.py
+    $ulab_test_dir/1d/complex/complex_sqrt.py
+    $ulab_test_dir/1d/complex/imag_real.py
+    $ulab_test_dir/1d/numpy/00smoke.py
+    $ulab_test_dir/1d/numpy/argminmax.py
+    $ulab_test_dir/1d/numpy/compare.py
+    $ulab_test_dir/1d/numpy/convolve.py
+    $ulab_test_dir/1d/numpy/fft.py
+    $ulab_test_dir/1d/numpy/gc.py
+    $ulab_test_dir/1d/numpy/interp.py
+    $ulab_test_dir/1d/numpy/optimize.py
+    $ulab_test_dir/1d/numpy/poly.py
+    $ulab_test_dir/1d/numpy/slicing.py
+    $ulab_test_dir/1d/numpy/slicing2.py
+    $ulab_test_dir/1d/numpy/sum.py
+    $ulab_test_dir/1d/numpy/trapz.py
+    $ulab_test_dir/1d/numpy/universal_functions.py
+    $ulab_test_dir/2d/complex/binary_op.py
+    $ulab_test_dir/2d/complex/complex_exp.py
+    $ulab_test_dir/2d/complex/complex_sqrt.py
+    $ulab_test_dir/2d/complex/conjugate.py
+    $ulab_test_dir/2d/complex/imag_real.py
+    $ulab_test_dir/2d/complex/sort_complex.py
+    $ulab_test_dir/2d/numpy/00smoke.py
+    $ulab_test_dir/2d/numpy/any_all.py
+    $ulab_test_dir/2d/numpy/arange.py
+    $ulab_test_dir/2d/numpy/asarray.py
+    $ulab_test_dir/2d/numpy/buffer.py
+    $ulab_test_dir/2d/numpy/cholesky.py
+    $ulab_test_dir/2d/numpy/concatenate.py
+    $ulab_test_dir/2d/numpy/delete.py
+    $ulab_test_dir/2d/numpy/diag.py
+    $ulab_test_dir/2d/numpy/eye.py
+    $ulab_test_dir/2d/numpy/full.py
+    $ulab_test_dir/2d/numpy/initialisation.py
+    $ulab_test_dir/2d/numpy/isinf.py
+    $ulab_test_dir/2d/numpy/linalg.py
+    $ulab_test_dir/2d/numpy/linspace.py
+    $ulab_test_dir/2d/numpy/logspace.py
+    $ulab_test_dir/2d/numpy/methods.py
+    $ulab_test_dir/2d/numpy/nonzero.py
+    $ulab_test_dir/2d/numpy/numericals.py
+    $ulab_test_dir/2d/numpy/ones.py
+    $ulab_test_dir/2d/numpy/operators.py
+    $ulab_test_dir/2d/numpy/signal.py
+    $ulab_test_dir/2d/numpy/size.py
+    $ulab_test_dir/2d/numpy/sort.py
+    $ulab_test_dir/2d/numpy/where.py
+    $ulab_test_dir/2d/numpy/zeros.py
+    $ulab_test_dir/2d/scipy/cho_solve.py
+    $ulab_test_dir/2d/scipy/solve_triangular.py
+    $ulab_test_dir/2d/scipy/sosfilt.py
+    $ulab_test_dir/2d/utils/from_buffer.py
+    $ulab_test_dir/3d/complex/complex_exp.py
+    $ulab_test_dir/3d/complex/complex_sqrt.py
+    $ulab_test_dir/3d/complex/imag_real.py
+    $ulab_test_dir/3d/numpy/create.py
+    $ulab_test_dir/4d/complex/complex_exp.py
+    $ulab_test_dir/4d/complex/complex_sqrt.py
+    $ulab_test_dir/4d/complex/imag_real.py
+    $ulab_test_dir/4d/numpy/create.py
+    "
+
 # Core MicroPython tests.
 ./run-tests.sh CORE_BASICS/*.py
 ./run-tests.sh CORE_UNICODE/*.py
@@ -11,7 +77,7 @@
 ./run-tests.sh cpydiff/*.py
 
 # micropython-ulab tests.
-./run-tests.sh `find ../lib/micropython-ulab/tests -name \*.py | sort`
+./run-tests.sh $ulab_tests
 
 # MicroPython performance tests (they have no expected output).
 ./run-perfbench.sh
