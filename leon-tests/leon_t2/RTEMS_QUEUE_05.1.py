58d590e8db3620f5f0bf07d3bab360e138767d0c
Damien George <dpgeorge@georgerobotics.co.uk>
2022-11-27 01:08:33 +1100

    leon-tests/leon_t2/RTEMS_QUEUE_05: Synchronise output and delete queue.
    Signed-off-by: Damien George <dpgeorge@georgerobotics.co.uk>

--- leon-tests/leon_t2/RTEMS_QUEUE_05.1.py
+++ leon-tests/leon_t2/RTEMS_QUEUE_05.1.py
@@ -9,13 +9,24 @@ script_id = '[T%u]' % rtems.script_id()
 
 mq = rtems.queue.create('tstq', 4, 10)
 buf = bytearray(10)
+result = None
 
 # below tests should not need the heap
 micropython.heap_lock()
 
 print(script_id, 'queue broadcast:', mq.broadcast('broadcast1'))
 print(script_id, 'queue pending:', mq.get_number_pending())
-time.sleep(2) # wait for script 2 to connect to queue
+
+time.sleep(2) # wait for script 2 to connect to receive
+
 print(script_id, 'queue broadcast:', mq.broadcast('broadcast2'))
-print(script_id, 'queue receive:', mq.receive(buf))
+
+result = mq.receive(buf)
+
+time.sleep(1) # wait for script 2 to finish printing
+
+print(script_id, 'queue receive:', result)
 print(script_id, 'queue buf:', buf)
+
+# clean up now that queue is no longer needed
+mq.delete()
--- leon-tests/leon_t2/RTEMS_QUEUE_05.2.py
+++ leon-tests/leon_t2/RTEMS_QUEUE_05.2.py
@@ -10,11 +10,16 @@ script_id = '[T%u]' % rtems.script_id()
 time.sleep(1)
 mq = rtems.queue.ident('tstq', 0)
 buf = bytearray(10)
+result = None
 
 # below tests should not need the heap
 micropython.heap_lock()
 
-print(script_id, 'queue receive:', mq.receive(buf))
+result = mq.receive(buf)
+
+time.sleep(2) # wait for script 1 to receive
+
+print(script_id, 'queue receive:', result)
 print(script_id, 'queue buf:', buf)
 buf[9] = ord('3')
 print(script_id, 'queue broadcast:', mq.broadcast(buf))
